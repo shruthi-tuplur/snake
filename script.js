b7e68b110e87afb723dc42e4808945c08a68d601
Shruthi Tuplur <shruthituplur@gmail.com>
2023-03-15 10:10:24 -0700

    tried new approach to project

--- script.js
+++ script.js
@@ -1,97 +1,187 @@
-let snakeHead = document.getElementById('snake-head');
-let board = document.getElementById('game-board');
-let foodX = Math.floor(Math.random() * 501);
-let foodY = Math.floor(Math.random() * 251)
 
-let foodNeg = Math.floor(Math.random())
+const table = document.getElementById('table');
+const startButton = document.getElementById('start');
 
-if (foodNeg < 0.5) {
-    foodY *= -1;
-}
 
- 
-let snakeCurrentRight = 0;
-let snakeCurrentUp = 250;
-let startButton  = document.getElementById('start');
+const numRows = 25;
+const numColumns = 25;
+
+const startingRow = Math.floor(numRows / 2);
+const startingColumn = Math.floor(numColumns / 2);
+
+let currentRow = startingRow;
+let currentColumn = startingColumn;
+
+let foodStartingRow = Math.floor(Math.random() * numRows);
+let foodStartingColumn = Math.floor(Math.random() * numColumns);
+
 let currentDirection = 'left';
-let food = document.getElementById('food').style;
+let numSegments = 4;
 
-food.top = `${foodX}`;
-food.right = `${foodY}`;
+let deleteColumn;
 
-console.log(food.top);
-console.log(food.right);
+window.onload = function() {
+    for (let i = 0; i < numRows; i++) {
+        let newRow = document.createElement('tr');
+        for (let j = 0; j < numColumns; j++) {
+            let newSquare = document.createElement('td');
 
-function moveSnake() {
+            if (i === foodStartingRow && j === foodStartingColumn) {
+                newSquare.classList.add('food');
+            }
 
-    if (currentDirection === 'left'){
-
-        snakeHead.style.right = `${snakeCurrentRight}px`
-        if (snakeCurrentRight >= 240){
-            console.log('hit wall');
-            return;
-        } else {
-            snakeCurrentRight += 15;
+            else if (i === startingRow && j === startingColumn) {
+                newSquare.classList.add('snake-body');
+                
+            } 
+            
+            newRow.appendChild(newSquare);
         }
-        
-    } 
-    else if (currentDirection === 'right'){
-        snakeHead.style.right = `${snakeCurrentRight}px`
-        if (snakeCurrentRight <= -240){
-            console.log('hit wall');
-            return;
-        } else {
-            snakeCurrentRight -= 15;
-        }
-        }
-    else if (currentDirection === 'up'){
-        snakeHead.style.top = `${snakeCurrentUp}px`
-        if (snakeCurrentUp <= 9){
-            console.log('hit wall');
-            return;
-        } else {
-            snakeCurrentUp -= 15;
-        }
-        
-        }
-
-    else if (currentDirection === 'down'){
-        snakeHead.style.top = `${snakeCurrentUp}px`
-        
-        if (snakeCurrentUp >= 490){
-            console.log('hit wall');
-            return;
-        } else {
-            snakeCurrentUp += 15;
-        }
-        
-        }    
-}
-
-function startGame() {
-    setInterval(moveSnake, 300);
+        table.appendChild(newRow);
+    }
 }
 
 
-function turnRight() {
-    currentDirection = 'right';
+
+
+
+function move() {
+
+    
+    snakeAte();
+    let currentSnakeRow = table.getElementsByTagName('tr')[currentRow];
+    let currentSnakeColumn = currentSnakeRow.getElementsByTagName('td')[currentColumn];
+
+    
+
+        if (currentDirection === 'left') {
+            for (let i = 1; i <= numSegments; i++) {
+
+                let newNum = currentColumn - 1
+                let newSnakeColumn = currentSnakeRow.getElementsByTagName('td')[newNum];
+                
+                newSnakeColumn.classList.add('snake-body');
+
+                deleteColumn = currentSnakeRow.getElementsByTagName('td')[newNum + numSegments];
+                deleteColumn.classList.remove('snake-body');
+
+                currentColumn -= 1;
+        }
+        }
+            
+        
+        
+    
+
+    else if (currentDirection === 'right') {
+        for (let i = 1; i <= numSegments; i++) {
+
+            let newNum = currentColumn + 1
+            let newSnakeColumn = currentSnakeRow.getElementsByTagName('td')[newNum];
+        
+            newSnakeColumn.classList.add('snake-body');
+
+            deleteColumn = currentSnakeRow.getElementsByTagName('td')[newNum - numSegments];
+            deleteColumn.classList.remove('snake-body');
+
+            currentColumn += 1;
+        }
+    }
+
+    else if (currentDirection === 'up') {
+        for (let i = 1; i <= numSegments; i++) {
+        let newRow = currentRow - 1;
+        let newSnakeRow = table.getElementsByTagName('tr')[newRow];
+        let newSnakeSquare = newSnakeRow.getElementsByTagName('td')[currentColumn]
+    
+        
+        newSnakeSquare.classList.add('snake-body');
+
+        let deleteRow = table.getElementsByTagName('tr')[newRow + numSegments];
+        let deleteSquare = deleteRow.getElementsByTagName('td')[currentColumn];
+        deleteSquare.classList.remove('snake-body');
+
+        currentRow -= 1;
+        }
+    }
+
+    else if (currentDirection === 'down') {
+        for (let i = 1; i <= numSegments; i++) {
+
+            let newRow = currentRow + 1;
+            let newSnakeRow = table.getElementsByTagName('tr')[newRow];
+            let newSnakeSquare = newSnakeRow.getElementsByTagName('td')[currentColumn]
+        
+            
+            newSnakeSquare.classList.add('snake-body');
+
+            let deleteRow = table.getElementsByTagName('tr')[newRow - numSegments];
+            let deleteSquare = deleteRow.getElementsByTagName('td')[currentColumn];
+            deleteSquare.classList.remove('snake-body');
+
+            currentRow += 1;
+
+    }
+    }
+}
+    
+
+    
+
+    
+
+
+function movingSnake() {
+    setInterval(move, 500);
+    ;
+}
+
+function turnUp() {
+    currentDirection = 'up';
+}
+
+function turnDown() {
+    currentDirection = 'down';
 }
 
 function turnLeft() {
     currentDirection = 'left';
 }
 
-
-function turnUp() {
-    currentDirection = 'up'
+function turnRight() {
+    currentDirection = 'right';
 }
 
 
-function turnDown() { // for what !!
-    currentDirection = 'down';
+
+function snakeAte() {
+    
+    if (currentRow ==  foodStartingRow && currentColumn == foodStartingColumn) {
+        
+        numSegments += 1;
+
+        let foodCurrentRow = table.getElementsByTagName('tr')[foodStartingRow];
+        
+        let foodCurrentSquare = foodCurrentRow.getElementsByTagName('td')[foodStartingColumn];
+        
+
+        console.log(foodCurrentRow)
+
+        foodCurrentSquare.classList.remove('food');
+        
+        foodStartingRow = Math.floor(Math.random() * numRows);
+        foodStartingColumn = Math.floor(Math.random() * numColumns);
+
+        let foodNewRow = table.getElementsByTagName('tr')[foodStartingRow];
+        let foodNewSquare = foodNewRow.getElementsByTagName('td')[foodStartingColumn];
+
+        foodNewSquare.classList.add('food');
+    }
+
 }
 
-startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', movingSnake);
+
 document.addEventListener('keydown', function(event) {
     if(event.code === 'ArrowDown'){
         turnDown();
