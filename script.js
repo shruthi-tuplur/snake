5aec3ffdd0ef3f90988a36cc9aa330277e786e2d
Shruthi Tuplur <shruthituplur@gmail.com>
2023-03-19 13:37:39 -0700

    adding color themes

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// SETTING GLOBAL VARIABLES
 
+// CONSTANTS
 const table = document.getElementById('table');
 const startButton = document.getElementById('start');
-
+const resetButton = document.getElementById('reset-button');
+const numSegmentsDisplay = document.getElementById('num-segments-display');
 
 const numRows = 25;
 const numColumns = 25;
@@ -9,45 +12,71 @@ const numColumns = 25;
 const startingRow = Math.floor(numRows / 2);
 const startingColumn = Math.floor(numColumns / 2);
 
-let snakeGrows = false;
+const windowBackground = document.getElementById('body');
+const allButtons = document.getElementsByTagName('button');
+const gameDiv = document.getElementById('game-div');
 
-let currentRow = startingRow;
-let currentColumn = startingColumn;
+// theme buttons
+
+buttons();
+
+// NON-CONSTANTS 
+
+// DEFAULT VALUES
+let snakeColors = [];
+let backgroundColor = '#44543D';
+let windowBackgroundColor = '#44543D'
+let windowColor = '#272727';
+let snakeGrows = false;
+let snakeColor = '#272727';
+let foodColor = '#A13939';
+let changeColorSquare;
+let colorSquare;
+let score = 0;
+let dead = false;
 let currentDirectionWord = 'left';
+let foodStartingRow = Math.floor(Math.random() * numRows);
+let foodStartingColumn = Math.floor(Math.random() * numColumns);
 let snake = {
     'body': [[startingRow, startingColumn]],
     'currentDirection': [0, -1]
 }
 
+// VARIABLES THAT CHANGE OFTEN 
+let body = snake['body'];
+let currentDirection = snake['currentDirection']
+let food;
+let scoreDisplay;
 
-let foodStartingRow = Math.floor(Math.random() * numRows);
-let foodStartingColumn = Math.floor(Math.random() * numColumns);
-
+// VARIABLES THAT SET THE GAME STATE
+let gameLoop;
 let gameState = {
     'food': [foodStartingRow, foodStartingColumn],
     'snake': snake
 }
 
-console.log(gameState['food']);
 
-let body = snake['body'];
-let snakeColor = '#272727';
-
-let currentDirection = snake['currentDirection']
-let dead = false;
+// --------------------------------------------------------------------------------------------------------------------------------------------------
+// FUNCTIONS TO SET/RESET GAME
+//window.onload = resetGame();
 
 
-let score = 0;
-let scoreDisplay;
-
-window.onload = function() {
+function resetGame() {
+    windowBackground.style.backgroundColor = windowBackgroundColor;
+    windowBackground.style.color = windowColor;
+    
     for (let i = 0; i < numRows; i++) {
         let newRow = document.createElement('tr');
         for (let j = 0; j < numColumns; j++) {
             let newSquare = document.createElement('td');
+            newSquare.style.backgroundColor = backgroundColor;
+            newSquare.style.borderColor = backgroundColor;
 
             if (i === foodStartingRow && j === foodStartingColumn) {
                 newSquare.classList.add('food');
+                newSquare.style.backgroundColor = foodColor;
+                
+                
             }
 
             else if (i === startingRow && j === startingColumn) {
@@ -60,16 +89,84 @@ window.onload = function() {
             newRow.appendChild(newSquare);
         }
         table.appendChild(newRow);
+
+        
     }
+
+    score = 0;
+    numSegments = 1;
+    snake['body'] = [[startingRow, startingColumn]];
+    body = snake['body'];
+    
+    snake['currentDirection'] = [0, -1];
+    currentDirection = snake['currentDirection'];
+
+    currentDirectionWord = 'left';
+    dead = false;
+
+
+
     scoreDisplay = document.getElementById('score');
     scoreDisplay.innerText = `SCORE: ${score}`;
 }
 
+function restartGame(){
+    clearInterval(gameLoop);
+    resetButton.style.display = 'none';
+    startButton.style.display = 'flex';
+    
+    numSegmentsDisplay.style.display = 'none';
+
+    for (let i= 0; i < numRows; i++){
+        for (let j = 0; j < numColumns; j++){
+            let square = table.getElementsByTagName('tr')[i].getElementsByTagName('td')[j];
+            square.style.backgroundColor = '#44543D';
+    }
+    }
+
+    for (let i = 0; i < 3; i++){
+        while (table.firstChild){
+            table.removeChild(table.firstChild);
+        }
+    }
+    resetGame();
+    
+    let text = document.getElementById('text');
+    text.innerText = 'SNAKE';
+
+    gameState['food'] = [foodStartingRow, foodStartingColumn];
+    
+}
+
+
+function died() {
+    let numSegments = body.length;
+    let text = document.getElementById('text');
+    text.innerText = 'U DIED :( '
+
+    let rightDiv = document.getElementById('right-div');
+    numSegmentsDisplay.style.display = 'flex';
+
+    if(numSegments === 1){
+    numSegmentsDisplay.innerText = `YOUR SNAKE HAD ${numSegments} SEGMENT WHEN IT DIED.`
+ } else {
+    numSegmentsDisplay.innerText = `YOUR SNAKE HAD ${numSegments} SEGMENTS WHEN IT DIED.`
+ }
+    rightDiv.appendChild(numSegmentsDisplay);
+    
+    clearInterval(gameLoop);
+}
+
+startButton.addEventListener('click', movingSnake);
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------
+// FUNCTIONS TO MOVE/GROW SNAKE
 
 function move() {
     checkAte();
     checkDied();
 
+    
     if (dead){
         died();
         return;
@@ -103,7 +200,7 @@ function move() {
 
     
     segToRemoveSquare.classList.remove('snake-body');
-    segToRemoveSquare.style.backgroundColor = '#44543D';
+    segToRemoveSquare.style.backgroundColor = backgroundColor;
     
     
     body.pop()
@@ -114,34 +211,66 @@ function move() {
 }
 
 function movingSnake() {
-    setInterval(move, 300);
+    resetGame();
+    gameDiv.style.display = 'flex';
+    let gameOptions = document.getElementById('game-options');
+    gameOptions.style.display = 'none';
+    startButton.style.display = 'none';
+    resetButton.style.display = 'flex';
+    resetButton.addEventListener('click', restartGame)
+    gameLoop = setInterval(move, 300);
+
+}
+
+function newFood(){
+    food = gameState['food'];
+    
+    let currentFoodRow = table.getElementsByTagName('tr')[food[0]];
+        let currentFoodSquare = currentFoodRow.getElementsByTagName('td')[food[1]];
+
+        currentFoodSquare.classList.remove('food');
+
+        gameState['food'] = [Math.floor(Math.random() * numRows), Math.floor(Math.random() * numColumns)];
+        food = gameState['food'];
+
+
+        let newFoodRow = table.getElementsByTagName('tr')[food[0]];
+        let newFoodSquare = newFoodRow.getElementsByTagName('td')[food[1]];
+
+
+        newFoodSquare.classList.add('food');
+        newFoodSquare.style.backgroundColor = foodColor;
+        
+        
 }
 
 
 function checkAte() {
-    let food = gameState['food'];
+    food = gameState['food'];
     let head = body[0];
 
+    if (head[0] === food[0] && head[1] === food[1] && score === 10) {
+        
+        snakeGrows = true;
+        score += 10;
+        scoreDisplay.innerText = `SCORE: ${score}`;
+
+        changeColor();
+
+    } else {
+    
     if (head[0] === food[0] && head[1] === food[1]) {
         
         snakeGrows = true;
         score += 10;
         scoreDisplay.innerText = `SCORE: ${score}`;
 
-        let currentFoodRow = table.getElementsByTagName('tr')[food[0]];
-        let currentFoodSquare = currentFoodRow.getElementsByTagName('td')[food[1]];
+        newFood();
 
-        currentFoodSquare.classList.remove('food');
-
-        gameState['food'] = [Math.floor(Math.random() * numRows), Math.floor(Math.random() * numColumns)]
-        food = gameState['food'];
-
-        let newFoodRow = table.getElementsByTagName('tr')[food[0]];
-        let newFoodSquare = newFoodRow.getElementsByTagName('td')[food[1]];
-
-        newFoodSquare.classList.add('food');
-
+    }
     } 
+
+    
 }
 
 function checkDied() {
@@ -152,25 +281,38 @@ function checkDied() {
             changeSquare.style.backgroundColor = snakeColor;
         }
         dead = true;
-        died();
-        
-        
         
     }
- else if ((head[1] === 0 && currentDirectionWord === 'left') || (head[1] === (numColumns - 1) && currentDirectionWord === 'right')) {
-    for (let i = 0; i < body.length; i++){
-        let changeSquare = table.getElementsByTagName('tr')[body[i][0]].getElementsByTagName('td')[body[i][1]];
-        changeSquare.style.backgroundColor = snakeColor;
-        
-    }
-        dead = true;
-        died();
-        
+    else if ((head[1] === 0 && currentDirectionWord === 'left') || (head[1] === (numColumns - 1) && currentDirectionWord === 'right')) {
+        for (let i = 0; i < body.length; i++){
+            let changeSquare = table.getElementsByTagName('tr')[body[i][0]].getElementsByTagName('td')[body[i][1]];
+            changeSquare.style.backgroundColor = snakeColor;
+            
+        }
+            dead = true;
        
- }
+    } else {
+        for (let i = 1; i < body.length; i++) {
+            if (head[0] == body[i][0] && head[1] == body[i][1]){
+                console.log('hello');
+                dead = true;
+            }
+        }
+    }
 }
 
-startButton.addEventListener('click', movingSnake);
+function changeColor(){
+    changeColorSquare = [Math.floor(Math.random() * numRows), Math.floor(Math.random() * numColumns)];
+
+
+    colorSquare = table.getElementsByTagName('tr')[changeColorSquare[0]].getElementsByTagName('td')[changeColorSquare[1]];
+    let colorSquareInd = Math.floor(Math.random() * snakeColors.length)
+    colorSquare.style.backgroundColor = snakeColors[colorSquareInd];
+    console.log(colorSquare);
+
+
+}
+
 
 document.addEventListener('keydown', function(event) {    
     if(event.code === 'ArrowDown'){
@@ -203,20 +345,56 @@ document.addEventListener('keydown', function(event) {
         currentDirectionWord = 'right';
         
     }
-    } else if(event.code === 'Space'){ 
-        console.log(body);
-        currentDirection = [0, 0];
     }
-
 })
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------
+// FUNCTIONS TO SET THEMES ETC.
 
-// NONPLAYER FUNCTIONS 
+function buttons(){
+    const defaultTheme = document.getElementById("default-theme");
+    const flowerChild = document.getElementById("flower-child");
+    const pacman = document.getElementById('pacman');
+    const nineties = document.getElementById("90s");
+    const glow = document.getElementById("glow");
 
-function died() {
+    flowerChild.addEventListener('click', function(){
+       flowerChild.style.backgroundColor = '#44543D';
+       flowerChild.style.color = '#272727';
+    })
 
-    let text = document.getElementById('text');
-    text.innerText = 'U DIED :( '
-    return;
+    defaultTheme.addEventListener('click', function(){
+        defaultTheme.style.backgroundColor = '#44543D';
+        defaultTheme.style.color = '#272727';
+     })
+
+    pacman.addEventListener('click', function(){
+        pacman.style.backgroundColor = '#44543D';
+        pacman.style.color = '#272727';
+
+        backgroundColor = 'black';
+        
+        windowBackgroundColor = 'black';
+        windowColor = 'white';
+        snakeColor = '#fdff00';
+        foodColor = 'white';
+        startButton.style.backgroundColor = '#0100F9';
+        startButton.style.color = 'white';
+        resetButton.style.backgroundColor = '#0100F9';
+        resetButton.style.color = 'white';
+
+        snakeColors = ['#ea82e5', '#46bfee', '#d03e19', '#db851c'];
+
+     })
+    
+    nineties.addEventListener('click', function(){
+        nineties.style.backgroundColor = '#44543D';
+        nineties.style.color = '#272727';
+     })
+
+    glow.addEventListener('click', function(){
+        glow.style.backgroundColor = '#44543D';
+        glow.style.color = '#272727';
+     })  
 }
+
